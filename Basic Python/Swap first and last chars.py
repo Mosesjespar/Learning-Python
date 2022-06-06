17fae1d3d16f6bb5552c238d6fee33bb8b3fbe73
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-06 23:02:19 +0300

    Create Swap first and last chars.py

---
+++ Basic Python/Swap first and last chars.py
@@ -0,0 +1,3 @@
+# Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
+def change_sring(str1):
+      return str1[-1:] + str1[1:-1] + str1[:1]
