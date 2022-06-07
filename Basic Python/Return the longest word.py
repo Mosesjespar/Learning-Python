bb16adf80d4f91e9c6be624eb3e180524c4345c8
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-07 18:25:43 +0300

    Dir change

--- Basic Python/Return the longest word.py
+++
@@ -1,8 +0,0 @@
-# Write a Python function that takes a list of words and return the longest word and the length of the longest one.
-
-def find_longest_word(words_list):
-    word_len = []
-    for n in words_list:
-        word_len.append((len(n), n))
-    word_len.sort()
-    return word_len[-1][0], word_len[-1][1]
