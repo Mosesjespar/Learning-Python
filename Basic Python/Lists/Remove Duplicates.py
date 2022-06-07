05d5afc7c526f5d86b019b75a44ee446050d1628
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-07 23:39:44 +0300

    Easy py

---
+++ Basic Python/Lists/Remove Duplicates.py
@@ -0,0 +1,4 @@
+# Write a Python program to remove duplicates from a list.
+def remove_dups(lis):
+    return list(set(lis))
+
---
+++ Basic Python/Lists/list clone.py
@@ -0,0 +1,3 @@
+# Write a Python program to clone or copy a list
+def list_copy(lis):
+    return lis.copy()
