564ee56bdaea7a69025cbc47fdd302ee12a844f5
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-11 22:49:30 +0300

    Create Reverse a string if it's length is a multiple of 4.py

---
+++ Basic Python/Strings/Reverse a string if it's length is a multiple of 4.py
@@ -0,0 +1,6 @@
+#  Write a Python function to reverses a string if it's length is a multiple of 4.
+
+def reverse_string(str1):
+    if len(str1) % 4 == 0:
+       return ''.join(reversed(str1))
+    return str1
